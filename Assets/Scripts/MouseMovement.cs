d672ab62d9651d19fbb02b7a9d505de7a3dbad8a
Teemu Turku <teemu.turku@pp2.inet.fi>
2019-09-12 21:19:01 +0300

    ding dongdung ding dong

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    [SerializeField]
+    Vector2 offset;
+    Vector3 currentMousePos;
+    Vector3 desiredPos;
+    Camera mainCam;
+
+    void Start()
+    {
+        mainCam = Camera.main;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        currentMousePos = Input.mousePosition;
+        desiredPos = mainCam.ScreenToWorldPoint(currentMousePos);
+        desiredPos.x += offset.x;
+        desiredPos.y -= offset.y;
+        desiredPos.z = 0.0f;
+        transform.position = desiredPos;
+    }
+}
